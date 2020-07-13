66fc0cb7aac8cc643c85f4f1c5b30114fa2e209d
Vittor Valler <vittorvc@gmail.com>
2020-07-13 11:39:36 -0300

    Reading recipe API

--- read_apis/views.py
+++ read_apis/views.py
@@ -4,8 +4,14 @@ import requests
 
 def index(request):
     response = requests.get('https://django-blog-corey-tutorial.herokuapp.com/api/posts-list/')
+    blog_api_data = response.json()
+
+    response = requests.get('https://appreceitas.herokuapp.com/api/lista-receitas/')
+    appreceitas_api_data = response.json()
+
     api_data = {
-        'posts': response.json()
+        'posts': blog_api_data,
+        'recipes': appreceitas_api_data
     }
 
     return render(request, 'read_apis/index.html', api_data)
